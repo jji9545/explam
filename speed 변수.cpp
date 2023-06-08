95284ff9a174ccc2c0987f0d0ad5c1afdf3a5137
mostermouse <whdls7946@gmail.com>
2023-06-09 01:22:17 +0900

    hello

---
+++ speed 변수.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int speed = 60;
+    int time = 2.5;
+    int distance = 60*2.5;
+
+    cout << "이동한 거리는" << distance << "km입니다." <<endl;
+    return 0;
+}
